cc5445008cb49746fcc8d44bdbad65db6c268cc8
Fernando Emanuel <78227397+femm34@users.noreply.github.com>
2025-03-04 00:06:56 -0600

    feat: linked list remove with param method

--- src/main/java/com/fecd/LinkedList.java
+++ src/main/java/com/fecd/LinkedList.java
@@ -23,10 +23,78 @@ public class LinkedList<T> {
 
     public void printList() {
         ListNode<T> current = head;
-        while (current.next != null) {
-            System.out.print(current.val + " -> ");
+        if (head == null) {
+            return;
+        }
+        while (current != null) {
+            System.out.print(current.val);
+            if (current.next != null) {
+                System.out.print(" -> ");
+            }
             current = current.next;
         }
-        System.out.println("null");
+        System.out.println();
     }
+
+    public void removeLast() {
+        if (head == null) {
+            return;
+        }
+
+        if (head.next == null) {
+            head = null;
+        } else {
+            ListNode<T> current = head;
+            while (current.next.next != null) {
+                current = current.next;
+            }
+            current.next = null;
+        }
+    }
+
+
+    public Object remove(T value) {
+        if (head == null) {
+            return "List is empty";
+        }
+
+        if (head.val.equals(value)) {
+            head = head.next;
+            return value;
+        }
+
+        ListNode<T> currentNode = head;
+        while(currentNode.next != null){
+            if(currentNode.next.val == value){
+                ListNode<T> nodeToRemove = currentNode.next;
+                currentNode.next = currentNode.next.next;
+                return nodeToRemove.val;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return "No elements found";
+    }
+
+
+    public Object get(T value) {
+        if (head == null) {
+            return "Emptu list";
+        }
+        if (head.val == value) {
+            return head.val;
+        }
+        ListNode<T> current = head;
+        while (current.next != null) {
+            if (current.next == value) {
+                return current.next.val;
+            }
+            current = current.next;
+        }
+        if (current.val == value){
+            return current.val;
+        }
+        return "No elements found";
+    }
+
 }
--- src/main/java/com/fecd/Main.java
+++ src/main/java/com/fecd/Main.java
@@ -1,13 +1,40 @@
 package com.fecd;
 
+import java.util.List;
+
 public class Main {
+
+    public static void iterateList(List<Integer> arreglo, int indice) {
+        if (indice == arreglo.size()) {
+            return;
+        }
+        System.out.println(arreglo.get(indice));
+        iterateList(arreglo, indice + 1);
+    }
+
     public static void main(String[] args) {
         LinkedList<Object> linkedList = new LinkedList<>();
 
         linkedList.add("asdas");
         linkedList.add(12312);
-        linkedList.add("12312");
-
+        linkedList.add("99999");
+//        linkedList.add("te amo Elia");
         linkedList.printList();
+//        linkedList.removeLast();
+        linkedList.printList();
+        System.out.println("element found:");
+        System.out.println(linkedList.get(12312));;
+        linkedList.printList();;
+        System.out.println("deleted value: " + linkedList.remove("99999"));
+        linkedList.printList();
+
+//        linkedList.printList();
+//        linkedList.removeLast();
+//        linkedList.printList();
+
+
+//        List<Integer> list = List.of(1, 2, 3, 4, 5);
+//
+//        iterateList(list, 0);
     }
 }
